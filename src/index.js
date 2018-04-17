405eca797f76f7c4223178c4811f3c2a35861f15
andrewkiri <andrewkirichok@gmail.com>
2018-04-17 14:55:38 +0200

    save

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import path from 'path';
-import fs from 'fs';
-
 const resolveAliases = () => {
   return {
     resolveId: (importee, importer) => {
-      console.log("\n${importee}");
-      console.log("\n${importer}");
+      console.log(`\n${importee}`)
+      console.log(`\n${importer}`)
     }
   }
 }
 
-export default resolveAlias;
+export default resolveAliases
